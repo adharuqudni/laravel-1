3528d14904c121fc1cc9b2980da7cf4b1a9db30b
adharuqudni <annas.adharuqudni@gmail.com>
2021-09-12 16:31:50 +0700

    first commit

---
+++ resources/views/berita.blade.php
@@ -0,0 +1,19 @@
+@extends('base.master')
+
+@section('sidebar')
+    <div class="">
+        <h1>ini sidebar</h1>
+    </div>
+@endsection
+
+@section('tengah')
+    <div class="">
+        <h1>ini tengah</h1>
+    </div>
+@endsection
+
+@section('kanan')
+    <div class="">
+        <h1>ini kanan</h1>
+    </div>
+@endsection
